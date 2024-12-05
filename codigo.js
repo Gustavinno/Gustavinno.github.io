b556db6c6bebd33437447d44b902ac2e19528f0e
Gustavinno <43863137+Gustavinno@users.noreply.github.com>
2024-12-05 09:08:35 +0100

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,30 +1,86 @@
-function ocultarPeliculas() {
+function ocultarDocumentales() {
     //1. Hacer desaparecer todas las películas
-    document.querySelectorAll(".pelicula").forEach((pelicula) => {
-        pelicula.style.display = "None";
+    document.querySelectorAll(".documentales").forEach((documentales) => {
+        documentales.style.display = "None";
     });
 }
-document.querySelector("#boton-drama").addEventListener("click", () => {
+
+document.querySelector("#boton-dinosaurios").addEventListener("click", () => {
     //1. Hacer desaparecer todas las películas
-    ocultarPeliculas();
+    ocultarDocumentales();
     //2. Mostrar la seleccionada
-    document.querySelectorAll(".drama").forEach((pelicula) => {
-        pelicula.style.display = "Block";
+    document.querySelectorAll(".dinosaurios").forEach((documentales) => {
+        documentales.style.display = "Block";
     });
 });
-document.querySelector("#boton-terror").addEventListener("click", () => {
+document.querySelector("#boton-desiertos").addEventListener("click", () => {
     //1. Hacer desaparecer todas las películas
-    ocultarPeliculas();
+    ocultarDocumentales();
     //2. Mostrar la seleccionada
-    document.querySelectorAll(".terror").forEach((pelicula) => {
-        pelicula.style.display = "Block";
+    document.querySelectorAll(".desiertos").forEach((documentales) => {
+        documentales.style.display = "Block";
     });
 });
-document.querySelector("#boton-comedia").addEventListener("click", () => {
+document.querySelector("#boton-oceanos").addEventListener("click", () => {
     //1. Hacer desaparecer todas las películas
-    ocultarPeliculas();
+    ocultarDocumentales();
     //2. Mostrar la seleccionada
-    document.querySelectorAll(".comedia").forEach((pelicula) => {
-        pelicula.style.display = "Block";
+    document.querySelectorAll(".oceanos").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+
+document.querySelector("#boton-montañas").addEventListener("click", () => {
+    //1. Hacer desaparecer todas las películas
+    ocultarDocumentales();
+    //2. Mostrar la seleccionada
+    document.querySelectorAll(".montañas").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+document.querySelector("#boton-deslaves").addEventListener("click", () => {
+    //1. Hacer desaparecer todas las películas
+    ocultarDocumentales();
+    //2. Mostrar la seleccionada
+    document.querySelectorAll(".deslaves").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+document.querySelector("#boton-minas").addEventListener("click", () => {
+    //1. Hacer desaparecer todas las películas
+    ocultarDocumentales();
+    //2. Mostrar la seleccionada
+    document.querySelectorAll(".minas").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+document.querySelector("#boton-ballenas").addEventListener("click", () => {
+    //1. Hacer desaparecer todas las películas
+    ocultarDocumentales();
+    //2. Mostrar la seleccionada
+    document.querySelectorAll(".ballenas").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+document.querySelector("#boton-volcanes").addEventListener("click", () => {
+    //1. Hacer desaparecer todas las películas
+    ocultarDocumentales();
+    //2. Mostrar la seleccionada
+    document.querySelectorAll(".volcanes").forEach((documentales) => {
+        documentales.style.display = "Block";
+    });
+});
+
+document.querySelector("#boton-todos").addEventListener("click", () => {
+        document.querySelectorAll(".documentales").forEach((documentales) => {
+            documentales.style.display = "Block";
+        });
+    });
+
+document.querySelector("#genero").addEventListener("change", () => {
+    let generoDocumental = document.querySelector("#genero").value;
+    ocultarDocumentales();
+    document.querySelectorAll("."+generoDocumental).forEach((documentales) => {
+        documentales.style.display = "Block";
     });
 });
